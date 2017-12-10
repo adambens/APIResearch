515422c9ea7ca324532c56fc35e7869001315d95
Adam Benson <adambens@umich.edu>
2017-12-10 01:49:52 -0500

    uploading fb portion

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 import json
+import urllib3
+import facebook
 import requests
 import hiddeninfo.py as secrets 
 
@@ -11,7 +13,19 @@ import hiddeninfo.py as secrets
 
 #API #1: Reddit
 
+
 #API #2: Facebook
+print("Welcome")
+access_token = None
+if access_token is None:
+    access_token = input("\nCopy and paste token from https://developers.facebook.com/tools/explorer\n>  ")
+
+graph = facebook.GraphAPI(access_token)
+events = graph.request(‘/search?q=Big%20Data&type=event&limit=21’)
+eventid = eventList[1][‘id’]
+
+attenders = requests.get(“https://graph.facebook.com/v2.7/"+eventid+"/attending?access_token="+token+”&limit=”+str(attenderscount)) 
+attenders_json = attenders.json()
 
 #API #3: New York Times
 
