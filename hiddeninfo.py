819e94578d031c80bd51f47169291d69443153c9
Adam Benson <adambens@umich.edu>
2017-12-09 21:44:29 -0500

    token information

--- hiddeninfo.py
+++ hiddeninfo.py
@@ -3,5 +3,7 @@
 #Adam Benson
 
 #Reddit Information
+#secret: NqvTg3r3erHioJcCkUbOBEznCC8
 #New York Times
+#Here's your API Key for the Article Search API: e668b6036f41453082f11da9252ff86f
 #Facebook
