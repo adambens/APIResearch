0881d6a68e3f0647c740e37a9a7eacf598865a7b
Adam <adambens@umich.edu>
2017-12-11 01:03:29 -0500

    NY Times Done

--- api.py
+++ api.py
@@ -55,6 +55,7 @@ except:
 ###################################################################################
 ###################################################################################
 #API #1: Reddit
+"""
 print("Welcome to the Reddit Analysis Portion of the project")
 
 reddit = praw.Reddit(client_id = hiddeninfo.reddit_id,
@@ -72,6 +73,7 @@ def get_subreddit_submissions(subred):
     else:
         print("making new request")
         response = reddit.subreddit(subred)
+
         REDDIT_CACHE_DICTION[subred] = response
         x = str(REDDIT_CACHE_DICTION)
         reddit_cache_file = open(REDDIT_CACHE, 'w')
@@ -79,36 +81,37 @@ def get_subreddit_submissions(subred):
         reddit_cache_file.close()
     return response
 
-
-subreddit = get_subreddit_submissions('bigdata') #big data subreddit
+redditinput = input("Enter subreddit 'ex)bigdata' : ")
+subreddit = get_subreddit_submissions(redditinput) #big data subreddit
 print("subreddit title: ", subreddit.title)
 print(type(subreddit))
 count = 0
 for sub in subreddit.top(limit=100): #for submission in top 100 submissions in subreddit
     if not sub.stickied:
         count += 1
+        print('total comments: ', sub.num_comments)
+        total_comments = sub.num_comments
+        print('submission created at: ', sub.created_utc)
+        submission_date = sub.created_utc
         print('submission score: ', sub.score) #score = likes - dislikes
+        submission_score = sub.score
         #print(sub.id)
         print('submission author: ', sub.author) #author = username
+        submission_author = sub.author
         #print(type(sub.author))
         y = str(sub.author)
-        #print(y)
         aredditor = reddit.redditor(y)
         try:
-            uprint(aredditor.link_karma)
+            uprint('link karma: ', aredditor.link_karma)
+            print('\n')
         except:
-            print("No Karma")
-        #akarma = aredditor.link_karma
-        #print(akarma)
-        #ascore == sub.score
-        #asubmission = sub.id
-        #comments = sub.comments.replace_more(limit=0)
+            print("No Karma\n")
 #print(count)
-
+"""
 ###################################################################################
 #API #2: Facebook
 print("Welcome to the Facebook Analysis Portion of the project")
-
+"""
 access_token = None
 if access_token is None: #get token from fb user in order to run this script
     access_token = input("\nCopy and paste token from https://developers.facebook.com/tools/explorer\n>  ")
@@ -129,22 +132,26 @@ def get_fb_events(topic):
         fb_cache_file.close()
     return events
 
-t = input('enter topic: ')
+t = input("Enter Topic 'ex: Big Data' : ")
 eventsl = get_fb_events(t)
 #eventsl = get_fb_events("Big Data")
 eventslist = eventsl['data']
-#uprint(eventslist)
+#eventlist = json.dumps(eventslist, indent= 4)
+uprint(eventslist)
 
 for x in eventslist:
     eventid = x['id'] #event id = unique identifier to access more information on the event
     uprint(eventid)
+    eventname = x['name']
+    uprint(eventname)
     try:
-        uprint('end time: ', x['end_time']) #time of event in formation YYYY-MM-DD + Time
+        endtime = x['end_time'] # example 2017-12-19T14:30:00+0100 
+        uprint('end time: ', endtime) #time of event in formation YYYY-MM-DD + Time
     except:
         print("No Time Specified")
-    try:                    # example 2017-12-19T14:30:00+0100 
-        y = x['place']
-        uprint('location: ', y['location']) #printing event location information if avaliable
+    try:                    
+        place = x['place']
+        uprint('location: ', place['location']) #printing event location information if avaliable
     except:
         print("no location avaliable")
     detailz = graph.get_object(id=eventid, fields = 'attending_count, declined_count, interested_count')
@@ -153,23 +160,13 @@ for x in eventslist:
     num_interested = detailz['interested_count']
     print('attending: ', num_attending)
     print('interested: ', num_interested, '\n')
-
 """
-###NEXT STEP = access event id to get specific event information
-# Store in Database #attending and #interested
-
-
-event1 = graph.get_object(id=eventid, fields='attending_count,can_guests_invite,category,cover,declined_count,description,end_time,guest_list_enabled,interested_count,is_canceled,is_page_owned,is_viewer_admin,maybe_count,noreply_count,owner,parent_group,place,ticket_uri,timezone,type,updated_time')
-attenderscount = event1['attending_count']
-declinerscount = event1['declined_count']
-interestedcount = event1['interested_count']
-maybecount = event1['maybe_count']
-noreplycount = event1['noreply_count']
-attenderscount = event1['attending_count']
+"""
 attenders = requests.get("https://graph.facebook.com/v2.7/"+eventid+"/attending?access_token="+access_token+"&limit="+str(attenderscount)) 
 attenders_json = attenders.json()
-
-
+"""
+###################################################################################
+###################################################################################
 #API #3: New York Times
 
 print("Welcome to the New York Times Analysis Portion of the project")
@@ -181,7 +178,7 @@ if nyt_key is None: #get token from nyt user in order to run this script
     nyt_key = input("\nCopy and paste API Key from https://developer.nytimes.com/\n>  ")
 
 
-
+#Question = where to implement range(10) in order to get 100 results
 def get_nyt_articles(subject):
 
     if subject in NYT_CACHE_DICTION:
@@ -189,10 +186,11 @@ def get_nyt_articles(subject):
         nyt_api = NYT_CACHE_DICTION[subject]
     else:
         print("Making new request")
+        #for x in range(100):
         params = {'api-key': nyt_key, 'q': subject,
                'fq' : "headline(\"Big Data\")",
                'fl': 'headline, keywords, pub_date, news_desk'}
-               #'page': str(x)}
+               'page': str(x)}
         nyt_api =  requests.get(nytbase_url, params = params)
         NYT_CACHE_DICTION[subject] = nyt_api
         x = NYT_CACHE_DICTION
@@ -211,6 +209,7 @@ data_articles = json.loads(articles.text)
 subject_articles = data_articles['response']['docs']
 
 keywords_dict = {}
+sections_dict = {}
 for item in subject_articles:
     headline = item["headline"]["main"]
     print(headline)
@@ -218,15 +217,30 @@ for item in subject_articles:
     print(publication_date)
     news_section = item.get("new_desk", "Section Unavaliable")
     print(news_section)
+    if news_section != 'Section Unavaliable':
+        sections_dict[news_section] = sections_dict.get(news_section, 0) + 1
     keywords_list = item["keywords"]
     if len(keywords_list) != 0:
         for piece in keywords_list:
             words = piece['value']
             keywords_dict[words] = keywords_dict.get(words, 0) + 1
 
-print(keywords_dict)
+#print(keywords_dict)
+#print(sections_dict)
 
+sorted_keywords = [(a, keywords_dict[a]) for a in sorted(keywords_dict,
+                    key = keywords_dict.get, reverse = True)]
+for k, v in sorted_keywords:
+    print(k, v)
 
+sorted_sections = [(a, sections_dict[a]) for a in sorted(sections_dict,
+                    key = sections_dict.get, reverse = True)]
+print('\n')
+
+for c, d in sorted_sections:
+    print(c, d)
+
+"""
 for x in range(10):
     nytbase_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
     params = {'api-key': nyt_key, 'q': 'big data',
@@ -245,3 +259,4 @@ for x in range(10):
     eventslist = events['data']
     uprint(eventslist)
 """
+
